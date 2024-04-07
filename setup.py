1fc5fcd4afc49e9478161ebce6635974cce8a385
Marcio Silva <marciosilvadrum@gmail.com>
2024-04-07 00:13:54 -0300

    creating setip.py file for build exe

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+build_exe_options = {
+    "packages": ["pytube", "threading"],
+    "include_files": ["youtube.ico"]
+}
+
+setup(
+    name="YouTube Downloader",
+    version="1.0",
+    description="Aplicação para baixar vídeos do YouTube na melhora qualidade possível.",
+    options={"build_exe": build_exe_options},
+    executables = [Executable("main.py", icon="youtube.ico")]
+)
