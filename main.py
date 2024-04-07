8412925fb88ae7f7ffff2a0de3c22d5435655fd1
Marcio Silva <marciosilvadrum@gmail.com>
2024-04-06 23:17:23 -0300

    download function done

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from pytube import YouTube
+# import os
 
 # recieving the YouTube link 
 link = input("Copie aqui o link do vídeo do YouTube que deseja baixar: ")
 yt = YouTube(link)
 
+# function to download the video in the highest rsolution
+def download_video(yt):
+    try:
+        # getting the highest resolution
+        yd = yt.streams.get_highest_resolution()
+        # downloading the video
+        yd.download()
+        print("Download Concluído!")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+
+download_video(yt)
 
 
 
-print(f"Title: {yt.title}")
